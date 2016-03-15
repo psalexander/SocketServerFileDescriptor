62cefe07778f672f3a196fa8c184a388f8b6bea0
alexander <psaaa@yandex.ru>
2016-03-15 18:29:16 +0400

    Добавлена реализация MediaDataSource

--- app/src/main/java/com/asinenko/alexander/tsextractortest/TestMediaDataSource.java
+++ app/src/main/java/com/asinenko/alexander/tsextractortest/TestMediaDataSource.java
@@ -8,17 +8,40 @@ import java.io.IOException;
 
 /**
  * Created by alexander on 15.03.16.
+ *
+ * https://medium.com/@jacks205/implementing-your-own-android-mediadatasource-e67adb070731#.peixmslnd
+ *
  */
 @TargetApi(Build.VERSION_CODES.M)
 public class TestMediaDataSource extends MediaDataSource {
+
+    private volatile byte[] videoBuffer;
+    private volatile boolean isDownloading;
+
+
     @Override
     public int readAt(long position, byte[] buffer, int offset, int size) throws IOException {
-        return 0;
+        synchronized (videoBuffer){
+
+            int length = videoBuffer.length;
+            if (position >= length) {
+                return -1; // -1 indicates EOF
+            }
+
+            if (position + size > length) {
+                size -= (position + size) - length;
+            }
+
+            System.arraycopy(videoBuffer, (int)position, buffer, offset, size);
+            return size;
+        }
     }
 
     @Override
     public long getSize() throws IOException {
-        return 0;
+        synchronized (videoBuffer) {
+            return videoBuffer.length;
+        }
     }
 
     @Override
